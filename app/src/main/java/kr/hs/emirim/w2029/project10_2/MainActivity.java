62d3947404aec96819ddbb82e5516c875703392d
효정이 <w2029@e-mirim.hs.kr>
2021-07-21 13:09:17 +0900

    Completion Change ViewFlipper Project Commit

--- app/src/main/java/kr/hs/emirim/w2029/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/project10_2/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import java.util.function.ToDoubleBiFunction;
 
 public class MainActivity extends AppCompatActivity {
     String[] imgNames = {"MAK1", "MAK2", "MAK3", "MAK4","MAK5", "MAK6", "MAK7", "MAK8", "MAK9"};
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setTitle(R.string.title);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < imgVIds.length; i++){
+        for(int i=0; i<imgNames.length; i++){
             final int index;
             index = i;
             image[index] = findViewById(imgVIds[index]);
@@ -30,19 +31,20 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     voteCount[index]++;
-                    Toast.makeText(getApplicationContext(), imgNames[index] + "총" + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                    Log.v("*** 129", imgNames[index]+"총"+voteCount[index]+"표" );
+                    Toast.makeText(getApplicationContext(), imgNames[index]+"총"+voteCount[index]+"표", Toast.LENGTH_SHORT).show();
                 }
             });
         }
-        Button btnFinish = findViewById(R.id.btn_done);
-        btnFinish.setOnClickListener(btnListener);
+        Button btnFinsh = findViewById(R.id.btn_done);
+        btnFinsh.setOnClickListener(btnListener);
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            Intent intent = new Intent(getApplicationContext(), ResultActivity2.class);
             intent.putExtra("voteCount", voteCount);
-            intent.putExtra("imgNames", imgNames);
+            intent.putExtra("imgNames",imgNames);
             startActivity(intent);
         }
     };
